231feab0ed296fe8760628ae8257af66536527f5
yike <2098933249.@qq.com>
2018-11-01 17:58:47 +0800

    add home

---
+++ models/credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+	cookieSecret:'abdef',
+	qq:{
+		user: '1334785356@qq.com',
+		pass: 'kqpkppucbnnxbahb'
+	}
+};
---
+++ yike.js
@@ -0,0 +1,68 @@
+var express = require('express');
+//添加post请求插件
+var bodyparser = require('body-parser');
+//添加文件传输插件
+var formidable = require('formidable');
+//添加凭证文件
+var credentials = require('./models/credentials');
+//引入邮箱插件
+var nodemailer = require('nodemailer');
+
+//创建传输方式
+var transporter = nodemailer.createTransport({
+	service: 'qq',
+	auth: {
+		user: credentials.qq.user,
+		pass: credentials.qq.pass
+	}
+});
+
+/*引入.hbs后缀文件格式*/
+var handlebars = require('express-handlebars').create({
+	defaultLayout: 'main',
+    extname: '.hbs',
+	helpers: {
+	       section: function(name, options){
+	            if(!this._sections) this._sections = {};
+	            this._sections[name] = options.fn(this);
+	            return null;
+	       }
+	    }
+});
+
+var app = express();
+
+//设置handlebars视图引擎
+app.engine('hbs',handlebars.engine);
+app.set('view engine', 'hbs');
+
+//添加static中间价
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+//socket.io引入
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//汇总路由文件routes.js
+require('./routes/home.js')(app);
+
+//定制404页面
+app.use(function(req,res,next){
+	res.status(404);
+	res.render('404');
+});
+
+//定制500页面
+app.use(function(err,req,res,next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.set('port',process.env.PORT || 3000);
+app.listen(app.get('port'),function(){
+	console.log('监听. http://localhost:' + app.get('port'));
+});
