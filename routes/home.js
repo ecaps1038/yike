771d8eec24eeb031bb2e4da193266be026fafa49
yike <2098933249.@qq.com>
2018-11-20 15:05:19 +0800

    test

--- routes/home.js
+++ routes/home.js
@@ -15,15 +15,6 @@ module.exports = function(app){
 	app.get('/login',function(req,res){
 		res.render('register');
 	});	
-	app.get('/test',function(req,res){
-		res.render('test');
-		if(req.session.userId==req.signedCookies.id){
-			console.log('相同的');
-		}else{
-			console.log('不同'+req.session.userId);
-		}
-		console.log('name'+req.session.username);
-	});	
 	app.post('/login',function(req,res){
 		var email = req.body.email;
 		var pswd = req.body.pswd;
